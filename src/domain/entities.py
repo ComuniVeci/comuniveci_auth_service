11fff0e34416241d197afc69a9d63348a465b69d
JulianAVG64 <julianavg2002@gmail.com>
2025-07-07 18:19:40 -0500

    Dependencia pydantic[email] faltante y correción datetime en entidad Usuario

--- src/domain/entities.py
+++ src/domain/entities.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 class User:
     def __init__(self, email, username, hashed_password):
         self.email = email
         self.username = username
         self.hashed_password = hashed_password
-        self.created_at = datetime.now(datetime.timezone.utc)
+        self.created_at = datetime.now(timezone.utc)
