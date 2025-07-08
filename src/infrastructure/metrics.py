54774a7be02203541f2ea0bc077fa5881154eedb
JulianAVG64 <julianavg2002@gmail.com>
2025-07-07 19:00:46 -0500

    Cambio para ver la observabilidad con prometheus

--- src/infrastructure/metrics.py
+++ src/infrastructure/metrics.py
@@ -4,7 +4,7 @@ def setup_metrics(app):
     instrumentator = Instrumentator(
         should_group_status_codes=False,
         should_ignore_untemplated=True,
-        should_respect_env_var=True,
+        should_respect_env_var=False,
         excluded_handlers=["/metrics", "/docs", "/openapi.json"],
         should_instrument_requests_inprogress=True,
     )
