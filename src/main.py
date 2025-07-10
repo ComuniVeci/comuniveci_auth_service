01e46a7173650ded0fc7b4aeffd4f2b3d4467593
JulianAVG64 <julianavg2002@gmail.com>
2025-07-09 20:53:56 -0500

    Configuración de CORS para comunicación con el front-end

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from src.infrastructure.api.routes import router as api_router
 from src.infrastructure.metrics import setup_metrics
@@ -6,6 +7,14 @@ app = FastAPI(
     title="Auth Service - ComuniVeci",
     description="Servicio de autenticación de usuarios para la plataforma ComuniVeci.",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # o ["*"] para todos los orígenes (solo en desarrollo)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Registrar rutas de la API
